908f98d43375876e6f65d7b36b877fee0e4c7a0b
damvanhoangbuu1 <damhoangbuu@gmail.com>
2020-07-03 15:51:27 +0700

    Chapter 13: User microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -33,11 +33,19 @@ module SessionsHelper
     session.delete(:user_id)
   end
 
-  def current_user?(user)
+  def current_user? user
     user && user == current_user
   end
 
   def store_location
     session[:forwarding_url] = request.original_url if request.get?
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = "Please log in!!"
+    redirect_to login_url
   end
 end
